1f7061cddd45fa00ad3e7a81e16a71ed3f2b6e54
Tejas Sabunkar <tsabunkar@gmail.com>
2019-01-27 16:37:49 +0530

    calculating total logic

--- src/app/invoices/component/invoice-view/invoice-view.component.ts
+++ src/app/invoices/component/invoice-view/invoice-view.component.ts
@@ -10,12 +10,25 @@ import { Invoice } from '../../models/invoice';
 export class InvoiceViewComponent implements OnInit {
 
   invoice: Invoice;
+  total: number;
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe((data: { invoiceFetchingFromUrl: Invoice }) => {
-      console.log(data.invoiceFetchingFromUrl);
+
       this.invoice = data.invoiceFetchingFromUrl;
+      if (typeof this.invoice['quantityControl'] !== 'undefined'
+        && typeof this.invoice['rateControl'] !== 'undefined') {
+        this.total = this.invoice['quantityControl'] * this.invoice['rateControl'];
+      }
+      let salesTax = 0;
+
+      if (typeof this.invoice['taxControl'] !== 'undefined') {
+        salesTax = this.total * this.invoice['taxControl'] / 100;
+      }
+      this.total += salesTax;
+      console.log(this.total);
     });
   }
 
